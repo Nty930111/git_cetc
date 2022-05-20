18756520199a691b77922a5509f1b6ef4677d15a
nty <tingyu-nie@nuaa.edu.cn>
2022-05-20 14:40:47 +0800

    modify by master branch

--- hello.java
+++ hello.java
@@ -1,7 +1,8 @@
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello git init");
-		System.out.println("modify by cetc28_nty");
+		System.out.println("modify by master branch");
 		System.out.println("modify by dev branch");
+		System.out.println("modify by master branch 2nd");
 	}
 }
